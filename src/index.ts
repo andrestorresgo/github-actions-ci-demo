50812fe79ed329957262ed2c63eb7e47d242acc4
andrestorresgo <andytorresgonz@gmail.com>
2026-05-13 16:48:56 -0400

    test:v2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import usersApp from "./users";
 
 const app = new Hono();
 
-app.get("/health", (c) => c.json({ status: "ok" }, 200));
+app.get("/health", (c) => c.json({ status: "ok", version: "v2.0.0" }, 200));
 
 app.route("/users", usersApp);
 
